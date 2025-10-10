d038cf0a55cd112223fc86171162a60e4a88e329
Shubhanshu yadav <shubhanshu_y@ce.iitr.ac.in>
2025-10-10 17:09:14 +0530

    fix bug in login route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -115,7 +115,7 @@ const loginUser = asyncHandler(async(req, res) => {
   // get the data from frontend
   const {email, username, password} = req.body
 
-  if(!username || !email) {
+  if(!username && !email) {
     throw new ApiError(400, "username or email is required")
   }
 
